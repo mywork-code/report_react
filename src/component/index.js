9b6e92ced8535f61e508c6104a4bb3487109330d
humingfei526 <humingfei526@lu.com>
2018-03-28 15:32:42 +0800

    增加build

---
+++ src/component/index.js
@@ -0,0 +1,8 @@
+import Footer from './Footer';
+import ScreenLoading from './ScreenLoading';
+const AyobaseUI = {
+  Footer,
+  ScreenLoading,
+}
+
+module.exports = AyobaseUI;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import AyobaseUI from './component'
+import AppContainer from './core/app/AppContainer';
+import PageBase from './core/app/PageBase';
+const Ayobase = {
+	AyobaseUI,
+  AppContainer,
+  PageBase,
+};
+
+module.exports = Ayobase;
