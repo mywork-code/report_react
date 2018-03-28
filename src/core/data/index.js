1e314a7982007c1175e844624ba8fb95d52faca4
humingfei526 <humingfei526@lu.com>
2018-03-28 18:15:31 +0800

    store

---
+++ src/core/data/index.js
@@ -0,0 +1,18 @@
+import LocalStore from './store/LocalStore';
+import MemoryStore from './store/MemoryStore';
+import SessionStore from './store/SessionStore';
+import LocalCache from './cache/LocalCache';
+import MemoryCache from './cache/MemoryCache';
+import SessionCache from './cache/SessionCache';
+import AbstractModel from './model/AbstractModel';
+
+
+export default {
+  LocalStore,
+  MemoryStore,
+  SessionStore,
+  LocalCache,
+  MemoryCache,
+  SessionCache,
+  AbstractModel,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import AyobaseUI from './component'
 import AppContainer from './core/app/AppContainer';
 import PageBase from './core/app/PageBase';
+import HttpClient from './core/http/HttpClient';
+
 const Ayobase = {
 	AyobaseUI,
   AppContainer,
