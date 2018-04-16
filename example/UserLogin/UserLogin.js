dc7470005e222b98a696a9d2b23b9413ea66918d
humingfei526 <humingfei526@lu.com>
2018-04-16 09:21:35 +0800

    fix

--- example/UserLogin/UserLogin.js
+++ example/UserLogin/UserLogin.js
@@ -5,6 +5,7 @@ import BenefitData from '../data/testData';
 const {
   PageBase,
 } = Ayobase;
+
 class UserLogin extends PageBase {
   constructor(props) {
     super(props);
@@ -23,13 +24,15 @@ class UserLogin extends PageBase {
   }
 
   render() {
-  	return(<div className="user-login"><h1 onClick={this.testClick}>fuck dssfhhdoifu</h1>
-      <table className="fictitious-keyboard" onClick={this.getSeesionData}><tbody>
+  	return(<div className="user-login"> 
+      <h1 onClick={this.testClick} className="clearfix">fghgfh</h1>
+      <table className="fictitious-keyboard example" onClick={this.getSeesionData}><tbody>
        <tr><td>1</td><td>1</td><td>1</td></tr>
        <tr><td>1</td><td>1</td><td>1</td></tr>
        <tr><td colSpan="2">1</td><td>1</td></tr>
       </tbody></table>
-
+      <p>1</p> <p>1</p> <p>1</p> <p>1</p> <p>1</p> <p>1</p> <p>1</p> <p>1</p>
+      <div className="b1"></div>
       </div>)
   }
 }
