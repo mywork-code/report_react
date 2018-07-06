d5fa7221fc116192f272e02faa26c6bb360f9b1a
fuchen <fuchen@apass.cn>
2018-07-06 13:41:59 +0800

    增加网络请求

--- src/core/http/ApassHttp.js
+++ src/core/http/ApassHttp.js
@@ -9,8 +9,12 @@ class ApassHttpClicent{
   static post(options,suc,fail){
 
     if (typeof options.params !== String){
-      options.params = JSON.stringify(options.params)
+      //TODO token ？？？
+      options.params['x-auth-token'] = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqd3Rfd2l0aG91dF9zcHJpbmdfc2VjdXJpdHkiLCJhdXRoIjoie1wibW9iaWxlXCI6XCIxMzUyNDg3NDc3N1wiLFwidXNlcklkXCI6XCI4MDA5NDY2M1wifSIsImV4cCI6MTUzMTQ1NDUxNX0.cZudZIoDU6JnBhrWLkH3jtHKHICMPb7TE1Z-ZETwhnivU81ZR-TG6Cd7OWKOyznilFk33cxFoiLr7XDAcq9aSQ';
+      options.params = JSON.stringify(options.params);
     }
+
+    options.params = encrypt.encryptAES(options.params);
 
     request.post(Apis.BASE_URL+Apis.BASE_SERVER+options.url)
       .accept('application/json')
