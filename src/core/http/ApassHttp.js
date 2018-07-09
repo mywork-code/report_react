17d16bfd5b075e67cc7a8fc104f88fe9e67f217f
luchenghao <834651203@qq.com>
2018-07-09 11:57:55 +0800

    修改冲突文件

--- src/core/http/ApassHttp.js
+++ src/core/http/ApassHttp.js
@@ -3,19 +3,14 @@ import encrypt from './encrypt';
 import Apis from './Apis';
 import Toast from 'antd-mobile/lib/toast';
 import 'antd-mobile/lib/toast/style/css';
-
 class ApassHttpClicent{
-
   static post(options,suc,fail){
-
     if (typeof options.params !== String){
       //TODO token ？？？
       options.params['x-auth-token'] = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqd3Rfd2l0aG91dF9zcHJpbmdfc2VjdXJpdHkiLCJhdXRoIjoie1wibW9iaWxlXCI6XCIxMzUyNDg3NDc3N1wiLFwidXNlcklkXCI6XCI4MDA5NDY2M1wifSIsImV4cCI6MTUzMTQ1NDUxNX0.cZudZIoDU6JnBhrWLkH3jtHKHICMPb7TE1Z-ZETwhnivU81ZR-TG6Cd7OWKOyznilFk33cxFoiLr7XDAcq9aSQ';
       options.params = JSON.stringify(options.params);
     }
-
     options.params = encrypt.encryptAES(options.params);
-
     request.post(Apis.BASE_URL+Apis.BASE_SERVER+options.url)
       .accept('application/json')
       .send({ "h5Data": options.params })
@@ -32,17 +27,12 @@ class ApassHttpClicent{
             if(resp.body.msg) Toast.info(resp.body.msg,1);
           }else if(resp.body.status == '-2'){
              //TODO  重新登录
-
-
           }else{
             fail && fail({status:"-1000",msg:'服务器位置的状态码'});
           }
           return;
         }
       });
-
   }
-
 }
-
 export default ApassHttpClicent;
--- src/pages/OperatingDetail/OperatingDetail.js
+++ src/pages/OperatingDetail/OperatingDetail.js
@@ -3,36 +3,22 @@ import AyoBase from '../../core';
 import './OperatingDetail.css';
 import mock from './mock';
 import ApassTable from '../../component/ApassTable/ApassTable'
-
-
-import ApassTable from '../../component/ApassTable/ApassTable'
 import {Apis,ApassHttp} from '../../core/index';
-
 const {
   PageBase,
 } = AyoBase;
-
 class OperatingDetail extends PageBase {
   constructor(props){
     super(props);
     this.state = {
-<<<<<<< HEAD
-       dataSource: mock.tableDate().dataSource,
-       columns: mock.tableDate().columns,
-=======
       dataSource: [],
       columns: [],
->>>>>>> acfcff14b4eb5f54b1bc17f9fe37d522d3ec85ef
     }
-
     console.log(this.state);
   }
-
   componentWillMount(){
     // console.log(this.getQuery())
-
   }
-
   componentDidMount(){
     const that = this
     ApassHttp.post({
@@ -50,14 +36,12 @@ class OperatingDetail extends PageBase {
           item.reportDateStr = that.fmtDate(item.reportDate)
         }
       })
-
       that.setState({
         dataSource:resp,
         columns:mock.tableDate().columns
       })
     })
   }
-
   fmtDate(obj){
     var date =  new Date(obj);
     var y = 1900+date.getYear();
@@ -65,7 +49,6 @@ class OperatingDetail extends PageBase {
     var d = "0"+date.getDate();
     return y+"-"+m.substring(m.length-2,m.length)+"-"+d.substring(d.length-2,d.length);
   }
-<<<<<<< HEAD
   componentDidMount(){
     ApassHttp.post({
       url:Apis.api.queryReport,
@@ -75,22 +58,13 @@ class OperatingDetail extends PageBase {
         "afDate": "2018-07-04"
       }
     })
-  }  
-  render() {
-    return (
-        <ApassTable dataSource={this.state.dataSource} columns={this.state.columns}/>
-               
-=======
-
+  }                
   render() {
     return (
       <div className="operation-box">
         <ApassTable dataSource={this.state.dataSource} columns={this.state.columns}/>
       </div>
-
->>>>>>> acfcff14b4eb5f54b1bc17f9fe37d522d3ec85ef
     )
   }
 }
-
 export default OperatingDetail;
